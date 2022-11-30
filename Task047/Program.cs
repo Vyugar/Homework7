df5d7d43ecba6e03c5350cc5f6ab2c4e9bb5d370
Vyugar <vyugar@yandex.ru>
2022-11-30 11:57:23 +0300

    aded Task047

--- Task047/Program.cs
+++ Task047/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+double[,] Create2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    double[,] array = new double[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+
+void print2dArray(double[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] array = Create2DArray(3, 4, 999);
+print2dArray(array);
